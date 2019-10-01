5fed6e5b9cb59b08e012c241acdb53f4a3625157
KartoonYoko <crashed9001@gmail.com>
2019-10-01 12:27:53 +0900

    Realize version

--- gaus/gaus/gaus.cpp
+++ gaus/gaus/gaus.cpp
@@ -13,18 +13,19 @@ using namespace std;
 
 void outm(vector<vector<double>> matrix, int n) {
 	cout.setf(ios::fixed);
+	const unsigned accuracy = 2;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (j == 4)
-				cout << "   " << matrix[i][j] << endl;
+		for (int j = 0; j < n + 1; j++) {
+			if (j == n)
+				cout << " |  " << setprecision(accuracy) << matrix[i][j] << endl;
 			else
 				if(matrix[i][j] < 0)
-					cout << "  " << setprecision(1) << matrix[i][j];
+					cout << "  " << setprecision(accuracy) << matrix[i][j];
 				else
-					cout << "   " << setprecision(1) << matrix[i][j];
+					cout << "   " << setprecision(accuracy) << matrix[i][j];
 		}
 	}
-	cout << "-------------" << endl;
+	cout << "-------------------------------------" << endl;
 	cout.unsetf(ios::fixed);
 }
 
@@ -34,47 +35,87 @@ void outm(vector<vector<double>> matrix, int n) {
 int main()
 {
 	const int n = 5;
-	vector <vector<double>> matrix(n, vector<double>(n, 1));
+	vector <vector<double>> matrix(n, vector<double>(n + 1, 1));
+	vector <vector<double>> nevyaz(n, vector<double>(n + 1, 1));
 	vector<double> answer(n);
+	double buf,  buf2;
+	double eps = 0.01;
 
+	matrix[0][1] = -0.66;
+	matrix[1][1] = -0.59;
+	matrix[2][1] = -0.8;
+	matrix[3][1] = -0.97;
+	matrix[4][1] = 0.33;
+	matrix[0][2] = 0.46;
+	matrix[1][2] = -0.46;
+	matrix[2][2] = -0.24;
+	matrix[3][2] = -0.46;
+	matrix[4][2] = -0.28;
+	matrix[0][3] = 0.62;
+	matrix[1][3] = 0.63;
+	matrix[2][3] = -0.51;
+	matrix[3][3] = 0.27;
+	matrix[4][3] = -0.94;
+	matrix[0][4] = 0.83;
+	matrix[1][4] = -0.68;
+	matrix[2][4] = 0.98;
+	matrix[3][4] = -0.45;
+	matrix[4][4] = -0.92;
+	matrix[0][0] = -0.02;
+	matrix[1][0] = 0.52;
+	matrix[2][0] = 0.28;
+	matrix[3][0] = 0.38;
+	matrix[4][0] = 0.17;
+
+	matrix[0][5] = 0.24;
+	matrix[1][5] = -0.98;
+	matrix[2][5] = -0.94;
+	matrix[3][5] = -0.84;
+	matrix[4][5] = -0.79;
+
+	nevyaz = matrix;
+
+	outm(matrix, n);
+	///*
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			matrix[i][j] = i + j + 2;
-		}
-	}
-	outm(matrix, n);
-	///*
-	double buf;
-	for (int i = 0; i < n - 1; i++) {
 		buf = matrix[i][i];
-		for (int j = 0; j < n; j++) {
-			if (buf != 0) {
-				matrix[i][j] /= matrix[i][i];
+		for (int j = i; j < n + 1; j++)
+		{
+			matrix[i][j] /= buf;
+		}
+		for (int k = i + 1; k < n; k++) {
+			buf2 = matrix[k][i];
+			for (int j = i; j < n + 1; j++)
+			{
+					matrix[k][j] -= buf2 * matrix[i][j];
 			}
-			else
-				cout << "A[" << i << "][" << i << "] == 0";
-			if (j == i) {
-				for (int k = i + 1; k < n; k++) {
-					matrix[k][j] = 0;
-				}
-			}
-			for (int i1 = i + 1; i1 < n; i1++) {
-				for (int j1 = 0; j1 < n; j1++) {
-					matrix[i1][j1] = (matrix[i1][j1] * buf - matrix[i1][i] * matrix[i][j1]) / buf;
-				}
-
+		}
+	}
+	for (int i = n - 1; i > -1; i--) {
+		for (int k = i - 1; k > -1; k--) {
+			buf = matrix[k][i];
+			for (int j = n; j > -1; j--)
+			{
+				matrix[k][j] -= buf * matrix[i][j];
 			}
 		}
 	}
 	outm(matrix, n);
+	for (int i = 0; i < n; i++) {
+		answer[i] = matrix[i][n];
+	}
 	///*
-	for (int i = n - 2; i > -1; i--) {
-		for (int j = n - 2; j > -1; j--) {
-			matrix[i][j] -= matrix[i + 1][j + 1] * matrix[i][i];
-		}
-	}//*/
 
-	outm(matrix, n);
+	
+	for (int i = 0; i < n; i++) {
+		buf = 0;
+		for (int j = 0; j < n; j++) {
+			buf += nevyaz[i][j] * answer[j];
+		}
+		if (abs(nevyaz[i][n] - buf) > eps) cout << "Not required accuracy in line " << i << endl;
+		else cout << "There is required accuracy in line " << i << endl;
+	}
+	
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
